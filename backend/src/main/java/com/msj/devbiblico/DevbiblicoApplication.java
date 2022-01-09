d2fd7b8cc704619b40cfe9e54ca15673fbf7fd7e
Mateus Souza <mateusssouzzaads@gmail.com>
2022-01-09 00:57:22 -0300

    Estrutura do backend

---
+++ backend/src/main/java/com/msj/devbiblico/DevbiblicoApplication.java
@@ -0,0 +1,13 @@
+package com.msj.devbiblico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbiblicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbiblicoApplication.class, args);
+	}
+
+}
