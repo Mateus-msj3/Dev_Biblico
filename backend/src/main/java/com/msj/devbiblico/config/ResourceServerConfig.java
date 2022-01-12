46e0534538d42b6b98daeb13be15808bd8aece8b
Mateus Souza <mateussouzaads@outlook.com>
2022-01-12 10:14:46 -0300

    configuração do resouce e authorization server

---
+++ backend/src/main/java/com/msj/devbiblico/config/ResourceServerConfig.java
@@ -0,0 +1,19 @@
+package com.msj.devbiblico.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/users/**").permitAll()
+                .anyRequest().denyAll();
+//                .antMatchers("/api/users/**").hasAnyRole("USER", "ADMIN");
+    }
+}
