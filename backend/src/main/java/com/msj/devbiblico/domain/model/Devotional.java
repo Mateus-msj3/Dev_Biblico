adfd737f65589bcd479f725ce16f6b5726b5495d
Mateus Souza <mateussouzaads@outlook.com>
2022-01-31 21:47:51 -0300

    feat: model devotional

---
+++ backend/src/main/java/com/msj/devbiblico/domain/model/Devotional.java
@@ -0,0 +1,41 @@
+package com.msj.devbiblico.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Devotional {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+    private String urlFoto;
+
+    private String titulo;
+
+    private String versiculo;
+
+    private Integer capitulo;
+
+    private String textoBiblico;
+
+    @OneToOne
+    private Book book;
+
+    @JsonIgnore
+    @CreationTimestamp
+    private LocalDateTime dataCadastro;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    private LocalDateTime dataAtualizacao;
+}
