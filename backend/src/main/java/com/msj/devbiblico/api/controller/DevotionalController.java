fae03fc894cef05db4f2c5641927a1799e84b8f4
Mateus Souza <mateussouzaads@outlook.com>
2022-02-01 21:54:10 -0300

    feat: controller devocional

---
+++ backend/src/main/java/com/msj/devbiblico/api/controller/DevotionalController.java
@@ -0,0 +1,50 @@
+package com.msj.devbiblico.api.controller;
+
+import com.msj.devbiblico.domain.model.Devotional;
+import com.msj.devbiblico.domain.service.DevotionalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/devotional")
+@CrossOrigin("http://localhost:4200/")
+public class DevotionalController {
+
+    @Autowired
+    private DevotionalService devotionalService;
+
+    @GetMapping
+    public List<Devotional> allDevotionals() {
+        return devotionalService.all();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Devotional> findById(@PathVariable Long id) {
+        Devotional devotional = devotionalService.devotionalId(id);
+        return ResponseEntity.ok().body(devotional);
+    }
+
+    @PostMapping
+    public ResponseEntity<Devotional> saveDevotional(@Valid @RequestBody Devotional devotional) {
+        devotional = devotionalService.create(devotional);
+        return ResponseEntity.status(HttpStatus.CREATED).body(devotional);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Devotional> update(@PathVariable Long id, @RequestBody Devotional devotional) {
+        devotional.setId(id);
+        devotional = devotionalService.alter(devotional);
+        return ResponseEntity.ok(devotional);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remove(@PathVariable Long id) {
+        devotionalService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
